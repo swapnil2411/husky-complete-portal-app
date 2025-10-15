8ba50d3342fe6ef713b3451305fdff7742773449
Swapnil2411 <swapnildarge65@gmail.com>
2025-10-15 15:01:37 +0530

    Code added

--- src/pages/users/Users.js
+++ src/pages/users/Users.js
@@ -27,11 +27,7 @@ const MoreVertical = ({ className }) => (
   </svg>
 );
 
-const UserPlus = ({ className }) => (
-  <svg className={className} fill="none" stroke="currentColor" viewBox="0 0 24 24">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
-  </svg>
-);
+
 
 const ChevronDown = ({ className }) => (
   <svg className={className} fill="none" stroke="currentColor" viewBox="0 0 24 24">
